4e0e64b72391261c6984283dc9d7d03ad193ad6a
Hydrorubys <basile.huguiesienco@gmail.com>
2025-02-01 00:39:12 +0100

    mise en place de la page album

--- lib/pages/index/albums.dart
+++ lib/pages/index/albums.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Albums extends StatelessWidget {
   @override
@@ -7,6 +8,31 @@ class Albums extends StatelessWidget {
         navigationBar: CupertinoNavigationBar(
           middle: Text('Albums'),
         ),
-        child: Center(child: Text('Albums')));
+        child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+            ),
+            children: List<Widget>.generate(15, (int i) {
+              return Builder(builder: (BuildContext context) {
+                // fait la grille des albums
+                return Container(
+                    margin: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ));
+              });
+            })));
+  }
+
+  Widget albumbutton() {
+    return IconButton(
+      icon: const Icon(Icons.volume_up),
+      tooltip: 'Increase volume by 10',
+      onPressed: () {
+        print("miaou");
+      },
+    );
   }
 }
